11d861e6f3e57ba050b0c4499b69a4e964a28f88
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2025-01-14 16:55:47 +0530

    initialize the project

---
+++ src/test/java/com/example/Airline_Reservation_System/AirlineReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Airline_Reservation_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
